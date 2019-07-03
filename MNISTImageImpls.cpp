7e0852a33ebf0987acfdf7686dea89ba3e114a40
chenyue9713 <cy422311@gmail.com>
2019-07-03 00:02:47 +0000

    Completed all of functions

--- MNISTImageImpls.cpp
+++ MNISTImageImpls.cpp
@@ -17,13 +17,14 @@ RandomMNISTImage::RandomMNISTImage(uint8 label):
 
 double RandomMNISTImage::distance(const MNISTImage *img) const
 {
-	return 0;
+	return ((double)(rand() % 714000)) / 100.0;
 }
 
 
 ArrayMNISTImage::ArrayMNISTImage(uint8 label):
 		MNISTImage(label)
 {
+	
 	this->label = label;
 	this->data = NULL;
 	this->size = 0;
@@ -46,7 +47,7 @@ FullArrayMNISTImage::FullArrayMNISTImage(uint8 data[], uint32 size, uint8 label)
 		ArrayMNISTImage(label)
 {
 	this->data = new uint8[size];
-	for(uint32 i=0; i < size; i++){
+	for (uint32 i = 0; i < size; i++) {
 		this->data[i] = data[i];
 	}
 
@@ -59,12 +60,34 @@ FullArrayMNISTImage::FullArrayMNISTImage(uint8 data[], uint32 size, uint8 label)
 DownsampleMNISTImage::DownsampleMNISTImage(uint8 data[], uint32 size, uint8 label, uint32 factor):
 		ArrayMNISTImage(label)
 {
+	uint32 new_size = size / factor;
+	this->data = new uint8[new_size];
+	for (uint32 i = 0; i < new_size; i++) {
+		this->data[i] = data[i*factor];
+	}
+
+	this->label = label;
+	this->size = new_size;
+
    
 }
 
 ScaledMNISTImage::ScaledMNISTImage(uint8 data[], uint32 numRows, uint32 numCols, uint8 label, uint32 factor):
 		ArrayMNISTImage(label)
 {
+	uint8 new_numRows = numRows / factor;
+	uint8 new_numCols = numCols / factor;
+	uint32 new_size = new_numRows * new_numCols;
+	this->data = new uint8[new_size];
+	for (uint8 i = 0; i < numRows; i++) {
+		for (uint8 j = 0; j < numCols; j++) {
+			this->data[(i/factor)*new_numRows + (j/factor)] += (data[(i* numRows) + j]) / (factor * factor);
+		}
+	}
+
+	this->size = new_size;
+	this->label = label;
+
     
 }
 
@@ -73,13 +96,29 @@ ScaledMNISTImage::ScaledMNISTImage(uint8 data[], uint32 numRows, uint32 numCols,
 SumMNISTImage::SumMNISTImage(uint8 data[], uint32 size, uint8 label):
 		MNISTImage(label)
 {
-	
+	double sum = 0;
+
+	for (uint32 i = 0; i < size; i++) {
+		sum += data[i];
+	}
+
+	this->sum = sum;
+
 }
 
 
 double SumMNISTImage::distance(const MNISTImage *img) const
 {
-	return 0;
+	const SumMNISTImage* arrayimg = dynamic_cast<const SumMNISTImage*>(img);
+	double first_img_sum = this->sum;
+	double second_img_sum = arrayimg->sum;
+	double diff = first_img_sum - second_img_sum;
+	double distance = sqrt((diff * diff));
+
+	return distance;
+
+
+
 }
 
 
